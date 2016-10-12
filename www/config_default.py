458f3e3f30373e5210ea3f3cd14ffb0f394d3bc4
atree <zj259410@126.com>
2016-10-12 12:28:00 +0800

    append GPL

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '681359',
+        'db': 'test1'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
